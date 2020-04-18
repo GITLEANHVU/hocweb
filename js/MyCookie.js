acd7b9f6f58e53875e88be7c4e4fb7aaeffc9ead
Lê Anh Vũ <leanhvu>
2020-04-18 23:00:24 +0700

    commit 2

---
+++ js/MyCookie.js
@@ -0,0 +1,40 @@
+function setCookie(cname, cvalue, exdays) {
+  console.log('setCookie', cname, cvalue);
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+  // them vao
+  console.log('setCookie exit: ', cname, document.cookie);
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+function checkCookie(cname) {
+  return getCookie(cname) !== '';
+
+  // var user = getCookie("username");
+  // if (user != "") {
+  //   alert("Welcome again " + user);
+  // } else {
+  //   user = prompt("Please enter your name:", "");
+  //   if (user != "" && user != null) {
+  //     setCookie("username", user, 30);
+  //   }
+  // }
+}
---
+++ js/MyUtils.js
@@ -0,0 +1,20 @@
+
+function submitLogin() {
+  let email = document.getElementById("inputEmailAddress").value;
+  let password = document.getElementById("inputPassword").value;
+
+  console.log('submitLogin : email = ', email);
+  console.log('submitLogin : Pwd = ', password);
+
+  sessionStorage.setItem('email', email);
+  sessionStorage.setItem('password', password);
+
+  // console.log('email = ' + sessionStorage.getItem('email'));
+  window.location.replace('index.html');
+}
+
+function logout() {
+  sessionStorage.setItem('email', '');
+  sessionStorage.setItem('password', '');
+  window.location.replace('login.html');
+}
