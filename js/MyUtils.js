eaaaee75ddcf344bf88ae0121848afd79f334107
Lê Anh Vũ <leanhvu>
2020-04-19 11:05:38 +0700

    login thiệt

--- js/MyUtils.js
+++ js/MyUtils.js
@@ -1,8 +1,10 @@
 
 function submitLogin() {
-  let email = document.getElementById("inputEmailAddress").value;
-  let password = document.getElementById("inputPassword").value;
+  // let email = document.getElementById("inputEmailAddress").value;
+  // let password = document.getElementById("inputPassword").value;
 
+  let email = $('#inputEmailAddress').val();
+  let password = $('#inputPassword').val();
   console.log('submitLogin : email = ', email);
   console.log('submitLogin : Pwd = ', password);
 
@@ -10,7 +12,25 @@ function submitLogin() {
   sessionStorage.setItem('password', password);
 
   // console.log('email = ' + sessionStorage.getItem('email'));
-  window.location.replace('index.html');
+  // window.location.replace('index.html');
+
+  $.post('http://codelaghien.club/web/login.asp', {
+    username: email,
+    password: password,
+  })
+    .done(function (returnedData) {
+      console.log(returnedData);
+      const myData = JSON.parse(returnedData);
+      console.log(myData);
+      if (myData['secret-key']) {
+        window.location.replace('index.html');
+      } else {
+        window.alert('khong thanh cong, dang nhap lai');
+      }
+    })
+    .fail(function (xhr, status, error) {
+      console.log(error);
+    });
 }
 
 function logout() {
